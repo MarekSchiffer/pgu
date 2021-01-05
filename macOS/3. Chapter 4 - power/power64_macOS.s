62a8144958e6ffd10c7c8499d0404c9e01bb391d
Marek Schiffer <schiffer@mareks-mbp.speedport_w_724v_09011603_06_007>
2021-01-05 20:44:40 +0100

    Updated macOS versions of power and factorial

---
+++ macOS/3. Chapter 4 - power/power64_macOS.s
@@ -0,0 +1,130 @@
+################################################################################
+#                                                                              #
+#              Programming From The Ground Up - Jonathan Bartlett              #
+#                                 Page 60 - 62                                 #
+#                     Typed in & Commented by Marek Schiffer                   #
+#                                                                              #
+#                                                                              #
+################################################################################
+
+# PURPOSE: Program to illustrate how functions work
+#          This program will compute the value of
+#          2^3 + 5^2
+#
+
+# Changes for 64 Bit:
+# 1.) All instruactions changed from long to quad, i.e. movl -> movq ...
+# 2.) stackspace for instructions doubled.
+
+# Changes for macOS:
+# .section had to disapear.
+# .type power, @function had to go as well.
+# In the future we might use C function convention _factorial
+# movl $60 is changed to $0x2000001 like before
+#
+
+
+# Everything in the main program is stored in registers, 
+# so the data section doesn't have anything.
+.data
+
+.text
+
+.global _start
+
+_start:
+	pushq $3		      # First we push the power on the stack
+	pushq $2		      # no the base
+	call power		      # Here we call power, This does two things.
+			              # 1. Push return address on the stack 2. Change eip to
+				      # first beginning of power funciton
+
+	addq $16, %rsp                 # move the stack pointer two values down so 3 and 2 will
+				      # be overwritten
+
+	pushq %rax		      # pushes the result %eax onto the stack to save it.
+
+	pushq $2		      # Same as above for 5^2
+	pushq $5
+	call power
+	addq $16, %rsp
+	
+	popq %rdi                     # Get the result from 2^3 from the stack intro %ebx
+	
+	addq %rax, %rdi               # add 2^3 and 5^2. Result is in %ebx
+
+	movq $0x2000001, %rax
+	syscall
+
+# PURPOSE: The function is used to compute
+#          the value of a number raiset to
+#          a power
+#
+# INPUT: First argument - the base number
+#	 Second argument - the power to raise it to
+#
+# OUTPUT: Will give the result as a return value
+#
+# NOTES: The power must be 1 or greater
+# 
+# VARIABLES: 
+#             %ebx - holds the base number
+#             %ecx - holds the power
+#
+#             -4(%ebp) - holds the current result
+#             %eax is used for temporary storage 
+#
+
+#.type power, @function
+
+power:
+	pushq %rbp                     # The base pointer goes on the stack
+	movq  %rsp, %rbp               # The base pointer gets replaced by the stack pointer
+	subq  $8, %rsp		       # Substract 4 from the stack pointer
+
+	movq 16(%rbp), %rbx             # Before the function is called, the power and then the 
+	movq 24(%rbp), %rcx            # base are pushed on the stack. Now we load the base in %ebx
+				       # and power in %ebx
+	movq %rbx, -8(%rbp)
+
+#Stack at this point:
+#     power 3           12(%ebp) = %ecx
+#  ^  base  2            8(%ebp) = %ebx
+#  |  Return Address    Invisible added by  call.
+#  |  ebp (Backup)	 <-ebp
+#  |  %ebx               <-esp
+#    
+power_loop_start:
+
+	cmpq $1, %rcx                  # End loop if power is 1
+	je end_power
+	
+	movq -8(%rbp), %rax            # move base from the stack intro %eax
+	imulq %rbx, %rax               # multiply base with base ans put result in %eax.
+	movq %rax, -8(%rbp)            # put new result on the stack
+	decq %rcx		       # remove power by 1.
+jmp power_loop_start                   # keep going untill power is 1
+	cmpq $1, %rcx
+	je end_power
+	
+	movq -8(%rbp), %rax
+	imulq %rbx, %rax
+	movq %rax, -8(%rbp)
+	
+	decq %rcx
+	jmp power_loop_start
+
+end_power:
+	movq -8(%rbp), %rax            # move the result from the stack in %eax
+
+	movq %rbp, %rsp                # move base pointer to stack pointer. %ebp never moved.
+	popq %rbp                      # remove old Base pointer from stack, esp now points to return address.
+	ret	                       # return  address to ip.
+
+#Stack at this point:
+#     power 3           12(%ebp) = %ecx
+#  ^  base  2            8(%ebp) = %ebx
+#  |  Return Address    Invisible added by call. <-esp
+#  |                    <-ebp 
+#  |  8
+
---
+++ macOS/4. Chapter 4 - factorial/factorial64_macOS.s
@@ -0,0 +1,141 @@
+################################################################################
+#                                                                              #
+#              Programming From The Ground Up - Jonathan Bartlett              #
+#                                 Page 66 - 68                                 #
+#                     Typed in & Commented by Marek Schiffer                   #
+#                                                                              #
+#                                                                              #
+################################################################################
+
+# PURPOSE - Given a number, this program computes the 
+#           factorial. For example, the factorial of
+#
+#           4!=4*3*2*1=24
+#
+
+# Changes for 64 Bit:
+# 1.) All instruactions changed from long to quad, i.e. movl -> movq ...
+# 2.) stackspace for instructions doubled.
+
+# Changes for macOS:
+# .section had to disapear
+# .type factorial, @function had to go as well
+# In the future we might use C function convention _factorial
+# movl $60 is changed to $0x2000001 like before
+#
+#	
+# This program shows, how to call a function recursively
+
+.data
+
+# This program has no gloaba data
+
+.text
+
+.global _start
+#.global factorial # this is unneeded unless we want to share
+		  # the function among other programs
+
+_start:                        # 01
+	pushq $4               # 02
+	call factorial         # 03
+	addq $8, %rsp          # 04
+                               # 05
+	movq %rax, %rdi        # 06
+	movl $0x2000001, %eax          # 07
+	syscall              # 08
+
+
+# This is the acutal function definition
+
+#.type factorial, @function     #09
+
+factorial:                     # 10	
+	pushq %rbp             # 11         # Push ebp onto the stack
+	movq %rsp, %rbp        # 12         # and copy esp to ebp
+	movq 16(%rbp), %rax     # 13         # get the value into eax. 8, because of the return address.
+
+	cmpq $1, %rax          # 14         # check for recusion anchor
+	je end_factorial       # 15         # jump to end, if the anchor holds true
+
+	decq %rax	       # 16 	# (if not) decrease n to (n-1)
+	pushq %rax             # 17         # push (n-1) onto the stack
+	call factorial         # 18         # call factorial with (n-1). The return address is
+                               # 19         # after the function call, here 19.          
+	movq 16(%rbp), %rbx     # 20        
+	imulq %rbx, %rax       # 21           
+
+end_factorial:                 # 22
+movq %rbp, %rsp                # 23         # This part will be executed for every function, after 
+popq %rbp                      # 24         # they returned
+ret                            # 25
+
+#   Stack until second call of		       eax      ebp    ip
+# 0100 4							02			
+# 0200 return address (# 04 )					03
+# 0300 Old %ebp (?)				4	300     11 
+# 0400 3 (n-1)					3	   	17
+# 0500 return address (# 19)			3		18
+# 0600 Old %ebp (300)				3		11
+# 0700 2 (n-2)					2	600	17	
+# 0800 return address ( # 19 )			2		18
+# 0900 Old %ebp (600)				2		18
+# 1000 1 (n-3)					1	900	17
+# 2000 return address ( # 19 )			1		18
+# 3000 Old %ebp (900)    <-ebp <-esp		1		18
+# 4000						1	3000    18
+
+
+# we reached 1!
+#
+# ip= 23
+# esp = ebp = 3000
+# popl ebp => ebp = 900 &esp = 2000
+# ret => esp = 1000 & ip = 19 & %eax = 1
+
+#   Stack until second call of		       eax      ebp    ip
+# 0100 4							02			
+# 0200 return address (# 04 )					03
+# 0300 Old %ebp (?)				4	        11 
+# 0400 3 (n-1)					3	300	17
+# 0500 return address (# 19)			3		18
+# 0600 Old %ebp (300)				3		11
+# 0700 2 (n-2)					2	600	17	
+# 0800 return address ( # 19 )			2		18
+# 0900 Old %ebp (600)	<- ebp			2		18
+# 1000 1 (n-3)		<- esp			1		17	
+
+# ebx = 2, imull %ebx, %eax => %eax=2    ip=21;
+# esp = ebp = 900
+# popl ebp => ebp =600 & esp = 800
+# ret => esp = 700 & ip=19 & eax=2
+
+#   Stack until second call of		       eax      ebp    ip
+# 0100 4							02			
+# 0200 return address (# 04 )					03
+# 0300 Old %ebp (?)				4	        11 
+# 0400 3 (n-1)					3	300	17
+# 0500 return address (# 19)			3		18
+# 0600 Old %ebp (300)		<- ebp		3		11
+# 0700 2 (n-2)			<- esp		2	600	17	
+
+# ebx = 3, imull %ebx, %eax => %eax=6    ip=21;
+# esp = ebp = 600
+# popl %ebp => %ebp = 300 & esp = 500 
+# ret => esp= 400 % ip =19 & eax =6
+
+#   Stack until second call of		       eax      ebp    ip
+# 0100 4							02			
+# 0200 return address (# 04 )					03
+# 0300 Old %ebp (?)		<- ebp		4	        11 
+# 0400 3 (n-1)			<- esp		3	300	17
+
+# ebx = 4, imull %ebx, %eax => %eax=24    ip=21;
+# ebp=esp=300
+# popl ebp => ebp = 200; esp=400
+# ret => esp = 300 & ip = 4 eax=24
+
+#   Stack until second call of		       eax      ebp    ip
+# 0100 4			<- esp				02			
+# 0200				<- ebp				03
+
