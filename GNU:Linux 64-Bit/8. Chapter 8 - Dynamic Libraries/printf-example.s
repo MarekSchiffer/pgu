bd20668ec9ddcb091ba9a3cf9a1ee34079607ea8
Marek Schiffer <schiffer@physik.fu-berlin.de>
2021-01-22 22:06:47 +0100

    Added printf-example

---
+++ GNU:Linux 64-Bit/8. Chapter 8 - Dynamic Libraries/printf-example.s
@@ -0,0 +1,41 @@
+# Changes for 64-Bit:
+# pushl => pushq
+
+# PURPOSE: This program is to demonstrate hoe to call printf
+# 
+
+.section .data
+
+# This string is called the fromat string. It's the first parameter,
+# and printf uses it to find out how many parameters it was given,
+# and what kind they are.
+
+firststring:
+ .ascii "Hello! %s is a %s who loves the number %d\n\0"
+name:
+ .ascii "Jonathan\0"
+personstring:
+ .ascii "person\0"
+ 
+# This could've also been and .equ, but we decided to give it
+# a real memory location just for kicks 
+
+numberloved:
+ .long 3
+
+.section .text
+
+.globl _start
+_start:
+# Note that the parameters are passed in the reverse order that they
+# are listed in the function's prototype.
+
+pushq numberloved		# This is the %d
+pushq $personstring		# This is the second %s
+pushq $name			# This is the first %s
+pushq $firststring		# This is the format string in the	
+				# prototype		
+call printf
+
+pushq $0
+call exit
