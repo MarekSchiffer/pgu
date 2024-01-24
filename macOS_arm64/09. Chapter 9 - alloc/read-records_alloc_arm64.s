4a7ff5a7567d0906e0a781f61fd9eb6cdc2082fa
MarekSchiffer <marek.schiffer@gmx.de>
2024-01-24 15:08:16 -0500

    read-records_alloc added

---
+++ macOS_arm64/09. Chapter 9 - alloc/read-records_alloc_arm64.s
@@ -0,0 +1,84 @@
+.include "macOS64.s"
+.include "record-def.s"
+
+.text
+.global _start1
+_start1:
+  mov x29, sp
+  sub sp, sp, #0x10
+  
+  bl allocate_init
+ 
+  mov x11, #record_size
+  str x11, [sp, -0x10]!
+  bl allocate
+  
+  adrp x12, record_buffer_ptr@page
+  add x12, x12, record_buffer_ptr@pageoff
+  str x0, [x12]
+
+//  bl write_newline
+//  bl write_newline
+
+
+// Open the file
+  mov x0, #-2
+  adrp x1, inputfile@page
+  add x1, x1, inputfile@pageoff
+  mov x2, #0
+  mov x3, #p_rw
+  mov x16, #sys_open
+  svc #0x80
+
+  str x0, [x29]   // Store fd on the stack
+
+   
+loop_output:
+// Read File in buffer
+   ldr x0, [x29]   // FD
+   adrp x11, record_buffer_ptr@page
+   add x11, x11, record_buffer_ptr@pageoff
+   stp x0, x11, [sp, -0x10]!
+   bl read_record
+
+   cmp x0, #record_size
+   b.ne output_loop_end
+
+   adrp x11, record_buffer_ptr@page
+   add x11, x11, record_buffer_ptr@pageoff
+   str x11, [sp, -0x10]!
+   bl count_char
+  
+   mov x12, x0
+
+// Write buffer to stdout
+   mov x2, x0
+   mov x0, #stdout 
+   adrp x1, record_buffer_ptr@page
+   add x1, x1, record_buffer_ptr@pageoff
+   mov x16, #sys_write
+   svc #0x80
+  
+   bl write_newline
+
+   bl loop_output
+output_loop_end:
+ // adrp x11, record_buffer_ptr@page
+ // add x11, x11, record_buffer_ptr@pageoff
+ // str x11, [sp, -0x10]!
+ // bl deallocate
+
+  ldr x0, [x29]   // FD
+  mov x16, #sys_close
+  svc #0x80
+
+  mov x16, #sys_exit
+  mov x0, x12
+  svc #0x80
+
+.data
+record_buffer_ptr: .quad 0
+.align 16     // Without an alignment here, the newline gets somehow 
+              // overwritten. To Do: Investigate why.
+inputfile:  .asciz "Dataoutput.dat"
+newline:    .asciz "\n"
