bb9ce90616921677eccdc9457c7bdcc509de77a7
MarekSchiffer <marek.schiffer@gmx.de>
2024-01-31 11:35:43 -0500

    Readme.md

--- GNU:Linux 32-Bit/02. Chapter 3 - maximum/Readme.md
+++ GNU:Linux 32-Bit/02. Chapter 3 - maximum/Readme.md
@@ -1,7 +1,7 @@
 # About
 This chapter uses the output to implement a linear search over a list of
-number. Due to the limitation of the 
-
+numbers to find the maximum. Due to the limitation of the output, the list
+is reduced to numbers between [0,255] until the completion of Chapter 10.
 
 ## Behind the Curtain
 Behind the curtain this chapter is about two things
@@ -18,7 +18,7 @@ data_items: .long 23,6,17,46,52,69
 ```
   movl $11, %edx
 ```
-is called immediate Mode. The number 11 comes directly from the RAM slot we write it in. This is a direct consequence of the Von Neuman architecture. 
+is called immediate Mode. The number 11 comes directly from the RAM slot we write it in. This is a direct consequence of the von Neumann architecture.
 ![alt text](https://github.com/MarekSchiffer/pgu/blob/main/GNU%3ALinux%2032-Bit/02.%20Chapter%203%20-%20maximum/Screenshots/Addressing32.png)
 If we had the address of an array, like your data\_items, we can insert it immediately in a register
 ```
@@ -57,7 +57,10 @@ hop forward in memory
 movq $data_items, %ecx
 movq 4(%ecx), %edx
 ```
-We now have the 6 in register ebx
+We now have the 6 in register ebx. Notice that the 4 gets "pulled in" the 
+parenthesis. Symbolically the expression gets evaluated to (%ecx+4) and the
+Indirect Addressing Mode gives back the value.
+
 ## Indexed Addressing Mode
 Additionally to the prefix offset, we can add a counting register with a multiplier:
 ```
@@ -77,11 +80,14 @@ movl $4, %edx
 movl data_items(%edx,%edi,4), %ebx
 ```
 Which again moves the 52 in register ebx
-This notation sucks! It sucks, because data\_items is outside of the parenthesis and yet gets evaluated as if data\_items was inside and after the address is calculated, the parenthesis make the movq jump to the right address. 
+This notation sucks! 
+data\_items is outside of the parenthesis and gets pulled in, like in the
+Base Pointer Addressing Mode. The expression symbolically evaluates to
+(data_items + %edx + 4*%edi).  
 
-
-
-
-
+# Branching
+The second introduction in this chapter is branching. The opcode cmp sets
+the %eflags register and according to the result the branch is made.
+The first branch is equivalent to a while loop and the second to an if statement
 
 
--- GNU:Linux 32-Bit/02. Chapter 3 - maximum/maximum32.s
+++ GNU:Linux 32-Bit/02. Chapter 3 - maximum/maximum32.s
@@ -7,14 +7,9 @@
 #                                                                              #
 ################################################################################
 
-# Purpose: This program finds the maximum number of a 
+# Purpose: This program finds the maximum number of a
 #          set of number (data items).
 #
-#         To assembly use
-#         as -32 exit.s -o exit.o
-#
-#         For linking:
-#         ld -m elf_i386 exit.o -o exit
 
 
 # Variables: The registers have the following uses:
@@ -24,36 +19,15 @@
 # $eax - Current data item
 
 # The following memory locatins are used:
-# 
+#
 # data_items - contains the item data. A "0" is used
 #              to terminate the data.
 
-# The idea is simple, we fill the first number in %ebx. %edi is the loop
-# counter, starting at 0. %ebx will constantly be updated, if the next value 
-# is bigger. If not kkjj
-
-.section .data
-
-# Notice a) data_items: has no .global option. Therefore it can only be used by this program
-#        b) .long is 4 bytes, Therefore 2^{32}= 4 294 967 296 numbers can be stored.
-#           other data types are .byte, .int, .ascii
-
-# INDEXED ADDRESSING MODE:
-# The most interesting thing here is the "Indexed Addressing Mode".
-# First note:
-# movl data_items, %eax is "Direct Addressing Mode", %eax holds the value (directly).
-# movl $data_items, %eax would load the address of data_items into %eax "Immediate Mode".
-#
-# Final Address = BeginningAddress(%Base_or_Offset,%IndexRegister,WordSizes)
-#
-# data_items(,%edi,4), data_items is the address of the list. the 0 item is 3
-# The Index Reigister will be calculated with %edi*4 and give back the value as in 
-# the "Direct Addresing Mode"
-
-
-# NOTE: 
+# NOTE:
 # If a comparsion is made the %eflags register will have a bit set and the je, jle will use
 # these bits. It's exactly as in J. Clark Scott - But How Do it know?
+
+.section .data
 
 data_items:
  .long 3,67,34,222,45,75,54,34,44,33,22,11,66,0
@@ -72,15 +46,15 @@ start_loop:
 	cmpl $0, %eax				# cmpl (compare long). Check if the current value is 0
 	je loop_exit                            # if equal (je =^ jump equal) yes, jump to exit.
 
-	incl %edi				# increase loop counter. 
+	incl %edi				# increase loop counter.
 	movl data_items(,%edi,4), %eax		# Same as above, check above.
 	cmpl %ebx, %eax
 	jle start_loop				# jle =^ jump less equal. If the next item is smaller than jump.
-	
+
 	movl %eax, %ebx				# If the next item is larger, change %ebx.
 jmp start_loop					# Wash, rinse, repeat.
 
-loop_exit:					# Same as exit.s. %ebx will be the exit status and has to be 
+loop_exit:					# Same as exit.s. %ebx will be the exit status and has to be
 	movl $1, %eax				# smaller than 255.
 	int $0x80
 
